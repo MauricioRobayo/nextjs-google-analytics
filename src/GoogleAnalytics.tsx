3eb70530eab1c80229412fd6fd98dc0405159366
Mauricio Robayo <rfmajo@gmail.com>
2021-08-26 11:08:34 -0500

    chore: change ga id var name
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/GoogleAnalytics.tsx
+++ src/GoogleAnalytics.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const isProduction = process.env.NODE_ENV === "production";
 
 type GoogleAnalyticsProps = {
-  gaTrackingId: string;
+  gaMeasurementId: string;
 };
 
 export function GoogleAnalytics({
-  gaTrackingId,
+  gaMeasurementId,
 }: GoogleAnalyticsProps): JSX.Element | null {
   if (!isProduction) {
     return null;
@@ -18,7 +18,7 @@ export function GoogleAnalytics({
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <script
         async
-        src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
       />
       <script
         dangerouslySetInnerHTML={{
@@ -26,7 +26,7 @@ export function GoogleAnalytics({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${gaTrackingId}', {
+            gtag('config', '${gaMeasurementId}', {
               page_path: window.location.pathname,
             });
           `,
