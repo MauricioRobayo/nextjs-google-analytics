f9412c2cf83ccd95595d2fcf4baa2175263912f9
Mauricio Robayo <rfmajo@gmail.com>
2021-08-28 09:18:24 -0500

    refactor: rename hook
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { GoogleAnalytics } from "./GoogleAnalytics";
-export { usePageView } from "./usePageView";
+export { usePagesViews } from "./usePagesViews";
 export { pageView, event } from "./gtag";
