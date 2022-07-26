5b9b91584cbf9fe450296b22be2d8c0e29dc5592
Pablo Ariel Mayobre <pablo.a.mayobre@gmail.com>
2022-07-25 23:55:29 -0300

    Add event handler for hashChangeComplete
    Track hash changes as page views

--- src/hooks/usePageViews.ts
+++ src/hooks/usePageViews.ts
@@ -14,9 +14,11 @@ export function usePageViews(gaMeasurementId?: string): void {
     };
 
     router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on('hashChangeComplete', handleRouteChange);
 
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off('hashChangeComplete', handleRouteChange);
     };
   }, [router.events]);
 }
