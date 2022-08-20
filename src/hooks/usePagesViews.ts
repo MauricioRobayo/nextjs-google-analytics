3f3dd6659a90902ad7b84cf6c931b2643596cd06
Mauricio Robayo <rfmaj@gmail.com>
2022-08-20 09:52:04 -0500

    Better warning message

--- src/hooks/usePagesViews.ts
+++ src/hooks/usePagesViews.ts
@@ -6,7 +6,7 @@ import { usePageViews, UsePageViewsOptions } from "./usePageViews";
  */
 export function usePagesViews(options?: UsePageViewsOptions): void {
   console.warn(
-    "The 'usePagesViews' hook is deprecated. Please use 'usePageViews' hook instead."
+    "Nextjs Google Analytics: The 'usePagesViews' hook is deprecated. Please use 'usePageViews' hook instead. https://github.com/MauricioRobayo/nextjs-google-analytics#readme"
   );
   usePageViews(options);
 }
