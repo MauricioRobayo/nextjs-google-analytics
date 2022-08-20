5ce17a413c439f9213eac215f5bc6cc28300e476
Mauricio Robayo <rfmaj@gmail.com>
2022-08-20 09:11:29 -0500

    Add deprecation warning

--- src/hooks/usePagesViews.ts
+++ src/hooks/usePagesViews.ts
@@ -5,5 +5,8 @@ import { usePageViews, UsePageViewsOptions } from "./usePageViews";
  * @deprecated Use usePageViews instead
  */
 export function usePagesViews(options?: UsePageViewsOptions): void {
+  console.warn(
+    "The 'usePagesViews' hook is deprecated. Please use 'usePageViews' hook instead."
+  );
   usePageViews(options);
 }
